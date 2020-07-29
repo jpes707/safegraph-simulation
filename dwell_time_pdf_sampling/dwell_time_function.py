52425247c58a96228a2a63cd209af8921831b707
rayan-yu <rayan.yu430@gmail.com>
2020-07-29 14:53:46 -0400

    universal_pdf_sampling

--- dwell_time_pdf_sampling/dwell_time_function.py
+++ dwell_time_pdf_sampling/dwell_time_function.py
@@ -1,6 +1,6 @@
 import numpy as np
 from distfit import distfit
-from scipy.stats import lognorm
+import scipy.stats as scipy
 
 # input distribution as two arrays:
 # nums: containing the numbers of people in each bucket_dwell range
@@ -16,7 +16,17 @@ def get_dwell_time(nums, buckets):
 
     dist = distfit()
     dist.fit_transform(filled_arr)
-    arg, loc_v, scale_v = float((dist.model['arg'])[0]), dist.model['loc'], dist.model['scale']
-    output_dwell_time = lognorm.rvs(arg, loc=loc_v, scale=scale_v, size=1)
+    dist_name = dist.model['name']
+
+    if not all(dist.model['arg']):
+        loc_v, scale_v = dist.model['loc'], dist.model['scale']
+        output_dwell_time = getattr(scipy, dist_name).rvs(loc=loc_v, scale=scale_v, size=1)
+    elif len(dist.model['arg']) == 2:
+        arg1, arg2, loc_v, scale_v = float((dist.model['arg'])[0]), float((dist.model['arg'])[1]), dist.model['loc'], \
+                                     dist.model['scale']
+        output_dwell_time = getattr(scipy, dist_name).rvs(arg1, arg2, loc=loc_v, scale=scale_v, size=1)
+    else:
+        arg, loc_v, scale_v = float((dist.model['arg'])[0]), dist.model['loc'], dist.model['scale']
+        output_dwell_time = getattr(scipy, dist_name).rvs(arg, loc=loc_v, scale=scale_v, size=1)
 
     return output_dwell_time
