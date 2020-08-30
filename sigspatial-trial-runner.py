1e64ef0f091fcce622b030f6d9f2932e3820f432
John Pesavento <jpes707@gmail.com>
2020-08-30 01:03:01 -0400

    Added random state overrides

--- sigspatial-trial-runner.py
+++ sigspatial-trial-runner.py
@@ -12,10 +12,17 @@ if len(sys.argv) > 2:
     delay_time = int(sys.argv[2]) * 60
 else:
     delay_time = int(input('Delay time before running in minutes (example: 180): ')) * 60
+
+if len(sys.argv) > 3:
+    rs = str(sys.argv[3])
+else:
+    rs = str(input('Override random state (type a positive integer to do so)? '))
+
 time.sleep(delay_time)
 
 for path in os.listdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config-files', 'sigspatial-trials')):
     if path.startswith(trial_category):
-        print(r'start "" py simulation.py sigspatial-trials\{}'.format(path[:-4]))
-        subprocess.call(r'start "" py simulation.py sigspatial-trials\{}'.format(path[:-4]), shell=True)
+        s = r'start "" py simulation.py sigspatial-trials\{} {}'.format(path[:-4], rs)
+        print(s)
+        subprocess.call(s, shell=True)
         time.sleep(60)
